a0f3bbc0d34ce8c5b30a0a6d46131230e682774f
brandonhru <79341156+brandonhru@users.noreply.github.com>
2023-04-18 17:42:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const nav = document.querySelector('nav');
+const toggle_btn = document.getElementById('toggle-btn');
+const content = document.querySelector('section');
+
+toggle_btn.onclick = function() {
+    nav.classList.toggle('hide');
+    content.classList.toggle('expand');
+};
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+
+
+const productContainers = [...document.querySelectorAll('.tutorial-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
